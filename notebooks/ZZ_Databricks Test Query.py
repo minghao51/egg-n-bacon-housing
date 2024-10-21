7da65631078d8586d2f3418901ce3d4516e0f406
minghao51 <minghao51>
2024-10-21 10:33:00 +0000

    Working databricks query

--- notebooks/ZZ_Databricks Test Query.py
+++ notebooks/ZZ_Databricks Test Query.py
@@ -4,6 +4,11 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## AI Query
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC SELECT ai_query(
 # MAGIC     'dbrx1',
@@ -21,6 +26,11 @@
 # COMMAND ----------
 
 # MAGIC %restart_python
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Raw OopenAI
 
 # COMMAND ----------
 
@@ -56,31 +66,36 @@ print(chat_completion.choices[0].message.content)
 
 # COMMAND ----------
 
-# from openai import OpenAI
-# # %pip install --upgrade typing_extensions
-# import os
+# MAGIC %md ## VectorSearch
 
-# # How to get your Databricks token: https://docs.databricks.com/en/dev-tools/auth/pat.html
-# # DATABRICKS_TOKEN = os.environ.get('DATABRICKS_TOKEN')
-# # Alternatively in a Databricks notebook you can use this:
-# DATABRICKS_TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+# COMMAND ----------
 
-# # client = OpenAI(
-# #   api_key=DATABRICKS_TOKEN,
-# #   base_url="https://dbc-0a534b63-c06b.cloud.databricks.com/serving-endpoints/dbrx1/invocations"
-# # )
+from langchain_databricks import DatabricksVectorSearch
 
-# client = OpenAI(
-#   api_key=DATABRICKS_TOKEN,
-#   base_url="https://dbc-0a534b63-c06b.cloud.databricks.com/serving-endpoints"
-# )
-# completions = client.completions.create(
-#   prompt='Write 3 reasons why you should train an AI model on domain specific data sets?',
-#   model="dbrx1",
-#   max_tokens=128
-# )
+vector_store = DatabricksVectorSearch(index_name="<YOUR_VECTOR_SEARCH_INDEX_NAME>")
+retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+retriever.invoke("What is Databricks?")
 
-# print(completions.choices[0].text)
+
+# COMMAND ----------
+
+# MAGIC %md ## ChatDatabricks
+
+# COMMAND ----------
+
+# MAGIC %pip install --upgrade langchain-databricks langchain
+
+# COMMAND ----------
+
+from langchain_databricks import ChatDatabricks
+
+chat_model = ChatDatabricks(
+    endpoint="dbrx1", #"databricks-meta-llama-3-1-70b-instruct"
+    temperature=0.1,
+    max_tokens=250,
+)
+chat_model.invoke("How to use Databricks?")
+
 
 # COMMAND ----------
 
