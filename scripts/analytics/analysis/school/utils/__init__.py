ead0c490587a95544bc7d81ab6aaa7425fd09b46
minghao <howt51@gmail.com>
2026-02-05 23:14:16 +0800

    feat: add RDDEstimator to utils package exports

--- scripts/analytics/analysis/school/utils/__init__.py
+++ scripts/analytics/analysis/school/utils/__init__.py
@@ -1,5 +1,6 @@
 """Shared utilities for enhanced school impact analysis."""
 
 from .spatial_validation import SpatialValidator
+from .rdd_estimators import RDDEstimator
 
-__all__ = ['SpatialValidator']
+__all__ = ['SpatialValidator', 'RDDEstimator']
